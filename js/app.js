e3cb59f05406bed0fe31b76a167209a2bd95bad7
Sandeep Suman <ssuman44@gmail.com>
2021-09-19 22:02:11 +0530

    Deployed e13809a with MkDocs version: 1.2.2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/fdp-python-2020/sw.js').then(function(registration) {
+  navigator.serviceWorker.register('/python-workshop/sw.js').then(function(registration) {
     console.log('ServiceWorker registration successful!');
   }).catch(function(err) {
     console.log('ServiceWorker registration failed: ', err);
